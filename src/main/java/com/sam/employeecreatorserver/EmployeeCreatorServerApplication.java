bf4a12cbb64d0ac5b4647b3c494982b5231d8ff5
Samuel Santos <107240729+samuel-santos91@users.noreply.github.com>
2023-11-20 21:23:14 +1100

    first commit

---
+++ src/main/java/com/sam/employeecreatorserver/EmployeeCreatorServerApplication.java
@@ -0,0 +1,13 @@
+package com.sam.employeecreatorserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeCreatorServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeCreatorServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sam/employeecreatorserver/EmployeeCreatorServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.employeecreatorserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeCreatorServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
