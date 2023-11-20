1b604062352a4b73106a60959e81b3465a2d515e
Samuel Santos <107240729+samuel-santos91@users.noreply.github.com>
2023-11-20 22:48:06 +1100

    adds EmployeeRepository

---
+++ src/main/java/com/sam/employeecreatorserver/employee/EmployeeRepository.java
@@ -0,0 +1,5 @@
+package com.sam.employeecreatorserver.employee;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {}
