95a585a9f6511c7eafc3081c77cb6a6c3873bdf3
Samuel Santos <107240729+samuel-santos91@users.noreply.github.com>
2024-02-15 19:15:58 +1100

    adds frontend url to allowed origins

--- src/main/java/com/sam/employeecreatorserver/config/WebConfig.java
+++ src/main/java/com/sam/employeecreatorserver/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer{
 
 	public void addCorsMappings(CorsRegistry registry) {
-		String[] allowedOrigins = {"http://localhost:3000/", "http://127.0.0.1:3000/"};
+		String[] allowedOrigins = {"http://localhost:3000", "http://127.0.0.1:3000", "https://employee-creator.vercel.app"};
 		
 		registry.addMapping("/**")
 		.allowedOrigins(allowedOrigins)
